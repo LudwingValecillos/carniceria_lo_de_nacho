08b1bb22b155b0daf991c4ecd9c19528eb92a119
LudwingValecillos <ludwingvaldev@gmail.com>
2025-02-07 21:52:11 -0300

    a

--- src/App.tsx
+++ src/App.tsx
@@ -182,7 +182,7 @@ const App: React.FC = () => {
                 </div>
               }
                 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+                <div className="grid justify-items-center items-center grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
                   {filteredProducts.map(product => (
                     <ProductCard
                       key={product.id}
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -13,26 +13,26 @@ const formatPrice = (price: number): string => {
     maximumFractionDigits: 2
   });
 };
+
 // Función para capitalizar la primera letra
 const capitalizeFirstLetter = (string: string): string => {
   return string.charAt(0).toUpperCase() + string.slice(1);
 };
+
 export function ProductCard({ product, onAddToCart }: ProductCardProps) {
   return (
-    <div className="bg-white rounded-lg shadow-md overflow-hidden border-2 border-gray-300" >
-      
+    <div className="bg-white rounded-lg shadow-md overflow-hidden md:w-56 h-72">
       <img
         src={product.image}
         alt={product.name}
-        className="w-full h-48 object-cover"
+        className="w-full h-48 object-contain"  // Cambiado a object-contain
       />
-      <div className="p-4">
-      <h3 className="text-lg font-semibold text-gray-800">
-  {capitalizeFirstLetter(product.name)}
-</h3>
-        <p className="text-sm text-gray-600 mb-2">{product.description}</p>
+      <div className="p-2 md:p-4">
+        <h3 className="text-lg font-semibold text-gray-800">
+          {capitalizeFirstLetter(product.name)}
+        </h3>
         <div className="flex items-center justify-between">
-          <span className="text-xl font-bold text-red-600">
+          <span className="md:text-xl font-bold text-red-600 mr-2">
             ${formatPrice(product.price)}/kg
           </span>
           <button
